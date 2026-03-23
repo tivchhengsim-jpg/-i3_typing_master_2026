c8d38bfaa41fc1409c05124855404e40276283e4
panhasary098-ux <panhasary098@gmail.com>
2026-03-23 22:39:40 +0700

    1&2

---
+++ first.java
@@ -0,0 +1,13 @@
+
+public class first {
+    public static void main(String[] args) {
+      System.out.println("---------- Welcome to i3 Typing Master v2026 ----------");
+      System.out.println("To begin, please select one of the following options:");
+      System.out.println("1. Login");
+      System.out.println("2. Register");
+      System.out.println("3. Exit");
+      System.out.print("Choose an option: ");
+
+    }
+    
+} 
---
+++ register.java
@@ -0,0 +1,9 @@
+public class register {
+    public static void main(String[] args) {
+        System.out.println("---------- Registration Page ----------");
+        System.out.println("Input a unique username: ");
+        System.out.println("Input password: **********");
+        System.out.println("Input confirm password: **********");
+        System.out.println("Registration successful! You can now log in with your credentials.");
+    }
+}
