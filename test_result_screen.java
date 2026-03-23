a79e159b07e68277b0eb2307615218fe7f3e85dd
tivchhengsim-jpg <tivchhengsim@gmail.com>
2026-03-23 10:36:19 +0700

    4_5_First upload

---
+++ test_result_screen.java
@@ -0,0 +1,17 @@
+public class test_result_screen{
+    public static void main(String[] args) throws Exception{
+        System.out.println("");
+        System.out.println("--------------- Typing Test Level 1Result --------------");
+        System.out.println("Speed (characters/min): 60");
+        System.out.println("(words/min): 40");
+        System.out.println("Total input characters: 200");
+        System.out.println("Total mistakes: 8");
+        System.out.println("--------------------------------------------------------");
+        System.out.println("1. Next level");
+        System.out.println("2. Logout");
+        System.out.println("3. Exit");
+        System.out.println("Choose an option: -");
+        
+    }
+}
+
---
+++ user_log_in_Screen.java
@@ -0,0 +1,7 @@
+public class user_log_in_Screen {
+    public static void main(String[] args) throws Exception {
+        System.out.println("--------------- Authentication --------------");
+        System.out.println("Username (left blank to cancel):");
+        System.out.println("Password: ********");
+    }
+}
