e34d3c6ab16caffa191271b499d0b27afd030d06
bunly-gif <vongbunly07@gmail.com>
2026-03-23 23:06:17 +0700

    Add files via upload

---
+++ practice.java
@@ -0,0 +1,14 @@
+public class practice {
+  public static void main(String[] args) {
+    System.out.println("\n-------------- Typing Test Level 1Result --------------");
+    System.out.println("Speed (characters/min): 60");
+    System.out.println("\t   (words/min): 40");
+    System.out.println("Total input characters: 200");
+    System.out.println("\tTotal mistakes: 8");
+    System.out.println("-------------------------------------------------------");
+    System.out.println("1. Next level");
+    System.out.println("2. Logout");
+    System.out.println("3. Exit");
+    System.out.println("Choose an option: ");
+  }
+}
---
+++ practice1.java
@@ -0,0 +1,10 @@
+public class practice1 {
+  public static void main(String[] args) {
+    System.out.println("\n-------------- Typing Test Level 1Result --------------");
+    System.out.println("Type the following paragraph(s) to test your typing speed"+
+                      "and quality (press ENTER to end the test and display the result): ");
+    System.out.println("ffff jjjj dddd kkkk ssss llll aaaa ;;;; fdsa jkl; fdsa jkl; asdf jkl; asdf");
+    System.out.println("jkl; sad dad had lad ask all fall hall flask shall gash hash flash glass glad");
+    System.out.println("flag sag hash lash. Ask a glad lad all a salad.");
+  }
+}
